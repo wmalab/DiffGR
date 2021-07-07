9b8ce978928f052b4cddc14728446a21d2010026
lhlruc <36868570+lhlruc@users.noreply.github.com>
2021-07-06 20:55:23 -0700

    Update DiffGR.R

--- DiffGR.R
+++ DiffGR.R
@@ -15,15 +15,15 @@
 #' @return a list that contains the tad result and genomic region result
 #' 
 #' The tad result table contains the following elements:
-#' tad.start: the starting locus of TAD
-#' tad.end: the end locus of TAD
+#' tad.start: the start location for the starting bin of TAD
+#' tad.end: the start location for the end bin of TAD
 #' scc: the SCC value of corresponding domain
 #' pvalue: the pvalue of differential testing on corresponding domain
 #' pvalue.adj: the adjusted pvalue of differential testing on corresponding domain (adjusted by Benjamin-Hochberg)
 #' 
 #' The genomic result table contains the following elements:
-#' genom.start: the starting locus of genomic region
-#' genom.end: the end locus of genomic region
+#' genom.start: the start location for the starting bin of genomic region
+#' genom.end: the start location for the end bin of genomic region
 #' condition.type: the type if candidate genomic region belonging to. 1:single-TAD, 2: Hierachical-TAD, 3: Alternating-TAD
 #' detect.result: the differential testing result for corresponding genomic region. 1:Differential 0:Non-differential 
 #'  
